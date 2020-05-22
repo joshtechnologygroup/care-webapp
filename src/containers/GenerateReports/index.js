f493d964904abcaf5d012e0596bd24d9c77a0642
Nishank Kumar <nishank.kumar@joshtechnologygroup.com>
2020-05-22 13:52:04 +0530

    generate report form

--- src/containers/GenerateReports/index.js
+++ src/containers/GenerateReports/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import Grid from '@material-ui/core/Grid';
+import Button from '@material-ui/core/Button';
 import CustomModal from 'Components/CustomModal';
 import ButtonToggle from 'Components/ButtonToggle';
+import CustomDateRange from 'Components/CustomDateRange';
 import { ReportType, Juridiction } from 'Constants/app.const';
+import useStyles from './styles';
 
 export const GenerateReports = (props) => {
+    const classes = useStyles();
     const { open, onClose } = props;
    
 
@@ -16,6 +20,14 @@ export const GenerateReports = (props) => {
         console.log('handleJuri--', selected);
     }
 
+    const generateReports = () => {
+        console.log('generateReports--');
+    }
+
+    const onRangeSelected = (range) => {
+        console.log('onRangeSelected--', range);
+    }
+
     return (
         <CustomModal open={open} onClose={onClose} title="Generate Reports">
              <Grid container spacing={3}>
@@ -24,6 +36,20 @@ export const GenerateReports = (props) => {
                 </Grid>
                 <Grid item xs={6}>
                     <ButtonToggle label="Select Juridiction" defaultSelected="PCMC" data={Juridiction} onChange={handleJuri} />
+                </Grid>
+                <Grid item xs={12}>
+                    <CustomDateRange label="Select Date OR Range" onChange={onRangeSelected} />
+                </Grid>
+                <Grid item xs={12}>
+                    <Button
+                        className={classes.button}
+                        variant="contained"
+                        color="primary"
+                        size="medium"
+                        onClick={generateReports}
+                    >
+                        Download
+                    </Button>
                 </Grid>
             </Grid>
         </CustomModal>
---
+++ src/containers/GenerateReports/styles.js
@@ -0,0 +1,9 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles(theme => ({
+  button: {
+    minWidth: '33rem'
+  },
+}));
+
+export default useStyles;
