58cbab0481a145b0b0969c8f0be88dedf96b9de9
ubuntu18 <runjeeth.nikam@joshtechnologygroup.com>
2020-05-23 17:15:56 +0530

    updated redux state

--- src/reducers/UserReducer.js
+++ src/reducers/UserReducer.js
@@ -2,12 +2,6 @@ import { USER } from 'Reducers/Types';
 
 const initialState = {
   id: null,
-  first_name:null,
-  last_name:null,
-  email: null,
-  age: null,
-  gender: null,
-  phone_number: null
 };
 
 export default function (state = initialState, action) {    
@@ -17,12 +11,6 @@ export default function (state = initialState, action) {
             return {
                 ...state,
                 id: action.data.id,
-                first_name: action.data.first_name,
-                last_name: action.data.last_name,
-                email: action.data.email,
-                age: action.data.age,
-                gender: action.data.gender,
-                phone_number: action.data.phone_number
             };
 
         default:
