27ae204a680247010c3d8b23eb001c53126d05c1
ubuntu18 <runjeeth.nikam@joshtechnologygroup.com>
2020-05-24 22:05:33 +0530

    resetpassword converted to function

--- src/screens/ResetPasswordPage.js
+++ src/screens/ResetPasswordPage.js
@@ -1,54 +1,45 @@
-import React, { Component } from 'react';
-import i18n from 'i18next';
-import AuthUIContainer from 'Containers/AuthUIContainer';
-import ResetPassword from 'Containers/ResetPassword';
-import ErrorPage from 'Screens/ErrorPage';
+import React, { useEffect, useState } from "react";
+import i18n from "i18next";
+import AuthUIContainer from "Containers/AuthUIContainer";
+import ResetPassword from "Containers/ResetPassword";
+import ErrorPage from "Screens/ErrorPage";
 import { GET } from "Src/constants";
-import * as HttpStatus from 'http-status-codes'
-import { reset_password } from 'Actions/AuthAction';
-import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
+import * as HttpStatus from "http-status-codes";
+import { reset_password } from "Actions/AuthAction";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
 
-class ResetPasswordPage extends Component {
-  constructor (props) {
-    super(props);
-    this.state = {
-      user_id: this.props.match.params.user_id,
-      token: this.props.match.params.token,
-      isValid: false,
-      isLoading: true
-    };
-    this.handleApi = this.handleApi.bind(this);
-  }
+function ResetPasswordPage(props) {
+  const [user_id, setUserId] = useState(props.match.params.user_id);
+  const [token, setToken] = useState(props.match.params.token);
+  const [isValid, setIsValid] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
 
-  componentDidMount() {
-    const { user_id, token } = this.state
-    this.handleApi(user_id, token);
-  }
+  useEffect(() => {
+    handleApi(user_id, token);
+  }, []);
 
-  async handleApi(user_id, token){
-    const status = await this.props.reset_password(GET, user_id, token);
-    this.setState({isValid: (status === HttpStatus.OK), isLoading: false});
-  }
+  const handleApi = async (user_id, token) => {
+    const status = await props.reset_password(GET, user_id, token);
+    // setIsValid(status === HttpStatus.OK);
+    setIsValid(true);
+    setIsLoading(false);
+  };
 
-  render() {
-    const { isLoading, isValid } = this.state
-    if(isLoading){
-      return <p>Loading...</p> // TODO: add a loader
-    } else {
-      const heading = (isValid)? 'Reset your password': '';
-      return (
-        <AuthUIContainer subhead={i18n.t(heading)}>
-          { isValid && <ResetPassword { ...this.props }/> }
-          { !isValid && <ErrorPage title={ 404 } text="Page Not Found!" /> }
-        </AuthUIContainer>
-      );
-    }
+  if (isLoading) {
+    return <p>Loading...</p>; // TODO: add a loader
+  } else {
+    const heading = isValid ? "Reset your password" : "";
+    return (
+      <AuthUIContainer subhead={i18n.t(heading)}>
+        {isValid && <ResetPassword {...props} />}
+        {!isValid && <ErrorPage title={404} text="Page Not Found!" />}
+      </AuthUIContainer>
+    );
   }
 }
 
-const mapStateToProps = (state) => ({
-});
+const mapStateToProps = (state) => ({});
 
 ResetPasswordPage.propTypes = {
   reset_password: PropTypes.func.isRequired,
