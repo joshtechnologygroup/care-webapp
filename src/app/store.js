d8df7ca656c2f408b1cc30eae9d7a3add3f4bb1b
Ayush Sidana <ayush.sidana@joshtechnolgygroup.com>
2020-05-27 21:39:19 +0530

    remaining part of adding new inventory integeration

--- src/app/store.js
+++ src/app/store.js
@@ -14,10 +14,7 @@ if(process.env.NODE_ENV !== 'development'){
 const store = createStore(
     rootReducer,
     initialState,
-    compose(
-        applyMiddleware(...middleware),
-        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-      )
+    compose(...args)
 );
 
 export default store;
