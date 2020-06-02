391b815b2fa2ea22d1c08de0cebf04ba87895334
shashank <shashank.verma@joshtechnologygroup.com>
2020-06-02 16:40:13 +0530

    remove unwanted files

--- src/app/store.js
+++ src/app/store.js
@@ -2,8 +2,6 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from 'Reducers';
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
-
 const initialState = {};
 
 const middleware = [thunk];
@@ -16,7 +14,7 @@ if(process.env.NODE_ENV !== 'development'){
 const store = createStore(
     rootReducer,
     initialState,
-    composeEnhancers(applyMiddleware(...middleware))
+    compose(applyMiddleware(...middleware))
 );
 
 export default store;
